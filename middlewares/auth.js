244130f666a85271f8958a5b30c92108cb30ec41
prakashwaddar628 <prakashwaddar628@gmail.com>
2025-05-08 23:39:45 +0530

    middleware initialized with jwt auth

---
+++ middlewares/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function (req, res, next) {
+    const token = req.headers.authorization?.split(' ')[1];
+
+    // Check for token
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+    try{
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = verified;
+        next();
+    }catch{
+        res.status(400).json({ message: 'Invalid token.' });
+    }
+}
